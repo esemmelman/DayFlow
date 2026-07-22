e0fceb0b07350b4ffcc00b5fe6592ef40a8b9433
esemmelman <90999990+esemmelman@users.noreply.github.com>
2026-07-22 06:48:57 -0700

    819

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // TEST
 
-// DayFlow v0.8-m18
+// DayFlow v0.8-m19
 
 let legacyTasks=JSON.parse(localStorage.getItem('df6')||'[]');
 let tasks=[...legacyTasks];
@@ -525,7 +525,7 @@ androidCal.onclick=()=>{
 androidAbout.onclick=()=>{
  if(!androidPanel.hidden&&androidPanel.querySelector('.android-about')){closeAndroidPanel();return;}
  androidPanel.hidden=false;
- androidPanel.innerHTML='<div class="android-about">DayFlow v0.8-m18</div>';
+ androidPanel.innerHTML='<div class="android-about">DayFlow v0.8-m19</div>';
 };
 prev.onclick=()=>{m--;if(m<0){m=11;y--;}drawCal();}
 next.onclick=()=>{m++;if(m>11){m=0;y++;}drawCal();}
