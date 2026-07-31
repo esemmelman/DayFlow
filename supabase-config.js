452e10e5f95c39a5c591eef5fbfd8a7c2d602679
esemmelman <90999990+esemmelman@users.noreply.github.com>
2026-07-30 17:26:50 -0700

    configjs

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 // The publishable (anon) key is safe in this client. Never put a service-role key here.
-window.DAYFLOW_SUPABASE = { url: 'https://ynfjfanvdvpyycoeweca.supabase.co', publishableKey: 'sb_publishable_j7q6Ox0GVsUv68D3oQiOBA_2Avx50il', vapidPublicKey: '' };
+window.DAYFLOW_SUPABASE = { url: 'https://ynfjfanvdvpyycoeweca.supabase.co', publishableKey: 'sb_publishable_j7q6Ox0GVsUv68D3oQiOBA_2Avx50il', vapidPublicKey: 'BAmNBTxY04jqXakoy1LJakg3yf-3FGnlbG6noOuhlCwcCoHJl5W5UhFD2w9zODBFCUSdtJIyYXiSEra1JHB2Uqk' };
