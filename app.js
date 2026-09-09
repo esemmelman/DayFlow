013dde9543035603502c84d6297d33c114d054e6
esemmelman <90999990+esemmelman@users.noreply.github.com>
2026-09-09 13:07:46 -0700

    Fix garbled close button symbols and bump to v0.8-m54

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // TEST
 
-// DayFlow v0.8-m53
+// DayFlow v0.8-m54
 
-const DAYFLOW_VERSION='v0.8-m53';
+const DAYFLOW_VERSION='v0.8-m54';
 document.title=`DayFlow ${DAYFLOW_VERSION}`;
 document.querySelector('.version').textContent=DAYFLOW_VERSION;
 
